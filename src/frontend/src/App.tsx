dbf8ab05e5d0bfdcfac85f6bffe5d532fa36e9f2
spencer <spencer.m.owens@gmail.com>
2025-01-21 16:23:29 -0800

    more cleanup

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import { ThemeProvider } from '../components/ThemeProvider'
 import { LoginPage } from '../pages/auth/LoginPage'
